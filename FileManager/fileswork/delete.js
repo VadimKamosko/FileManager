259b15a20127a3b101d466e761ac10b65fd9d668
VadimKamosko <76937121+VadimKamosko@users.noreply.github.com>
2022-06-12 20:15:08 +0300

    Delete FileManager directory

--- FileManager/fileswork/delete.js
+++
@@ -1,8 +0,0 @@
-// const fs = require("fs");
-import fs from 'fs'
-
-export const remove = async (filepath) => {
-  fs.unlink(filepath, (err) => {
-    if (err) console.log("Delete error");
-  });
-};
